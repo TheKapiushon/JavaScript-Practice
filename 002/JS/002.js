2b8f62277550e9a57acf99a74ddfbc1ab45c23ef
TheKapiushon <amirabasnorouzi25@gmail.com>
2024-02-20 16:58:35 +0330

    Add Separate Price function

---
+++ 002/JS/002.js
@@ -0,0 +1,11 @@
+// Separate Price
+
+function Price(Num) {
+
+    alert(Intl.NumberFormat('en-US').format(price));
+
+}
+
+const price = prompt("Enter your price: ");
+
+const result = Price(price);
