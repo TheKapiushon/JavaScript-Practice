385505716ffc2e9a016959efcf6b09ac0df5ace7
TheKapiushon <amirabasnorouzi25@gmail.com>
2024-02-20 17:01:30 +0330

    Add Month to Season function

--- 002/JS/002.js
+++ 002/JS/002.js
@@ -22,3 +22,27 @@ const Input = prompt("Enter your number: ");
 const Final = Convertor(Input);
 alert(Final);
 alert(typeof Final);
+
+// Month to Season
+
+function MonthToSeason(Month) {
+    if (Month >= 1 && 3 >= Month) {
+        return alert("Your season is: Spring, Fisrt season")
+    }
+
+    if (Month >= 4 && 6 >= Month) {
+        return alert("Your season is: Summer, Second season")
+    }
+
+    if (Month >= 7 && 9 >= Month) {
+        return alert("Your season is: Fall, Third season")
+    }
+
+    if (Month >= 10 && 12 >= Month) {
+        return alert("Your season is: Winter, Fourth season")
+    }
+}
+
+const Season = Number(prompt("Enter your month number: "));
+const Last = MonthToSeason(Season);
+alert(Last);
