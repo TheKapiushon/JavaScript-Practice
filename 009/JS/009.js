7e1252b97dc431ca3c2056e792be37e90e0ab8d4
AmirAbbas <157972493+TheKapiushon@users.noreply.github.com>
2024-04-03 18:48:32 +0330

    Add Header scroll function

---
+++ 009/JS/009.js
@@ -0,0 +1,7 @@
+// Header Scroll
+
+window.addEventListener("scroll", function(){
+
+    let header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
